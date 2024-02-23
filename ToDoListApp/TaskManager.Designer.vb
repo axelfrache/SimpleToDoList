f3754392247675f26acf1118f3f3dd8c322bfa55
axelfrache <axelfrache@gmail.com>
2024-02-23 12:44:38 +0100

    Init

---
+++ ToDoListApp/TaskManager.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class TaskManager
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBoxTaskName = New TextBox()
+        ButtonImportJson = New Button()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        GroupBox2 = New GroupBox()
+        ButtonExportJson = New Button()
+        TreeViewTasks = New TreeView()
+        GroupBox1 = New GroupBox()
+        ButtonAddChild = New Button()
+        ButtonDeleteChecked = New Button()
+        ButtonAddParent = New Button()
+        TableLayoutPanel1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBoxTaskName
+        ' 
+        TextBoxTaskName.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        TextBoxTaskName.BorderStyle = BorderStyle.FixedSingle
+        TextBoxTaskName.Location = New Point(3, 22)
+        TextBoxTaskName.Margin = New Padding(3, 3, 3, 25)
+        TextBoxTaskName.Name = "TextBoxTaskName"
+        TextBoxTaskName.Size = New Size(528, 39)
+        TextBoxTaskName.TabIndex = 0
+        ' 
+        ' ButtonImportJson
+        ' 
+        ButtonImportJson.Location = New Point(6, 44)
+        ButtonImportJson.Name = "ButtonImportJson"
+        ButtonImportJson.Size = New Size(150, 44)
+        ButtonImportJson.TabIndex = 3
+        ButtonImportJson.Text = "Import"
+        ButtonImportJson.UseVisualStyleBackColor = True
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.ColumnCount = 2
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 60.3960381F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 39.6039619F))
+        TableLayoutPanel1.Controls.Add(GroupBox2, 0, 2)
+        TableLayoutPanel1.Controls.Add(TextBoxTaskName, 0, 0)
+        TableLayoutPanel1.Controls.Add(TreeViewTasks, 0, 1)
+        TableLayoutPanel1.Controls.Add(GroupBox1, 1, 1)
+        TableLayoutPanel1.Location = New Point(12, 12)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 3
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 17.1577129F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 82.8422852F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 114.0F))
+        TableLayoutPanel1.Size = New Size(909, 616)
+        TableLayoutPanel1.TabIndex = 4
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        GroupBox2.Controls.Add(ButtonExportJson)
+        GroupBox2.Controls.Add(ButtonImportJson)
+        GroupBox2.Location = New Point(3, 504)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(543, 109)
+        GroupBox2.TabIndex = 9
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Tools"
+        ' 
+        ' ButtonExportJson
+        ' 
+        ButtonExportJson.Location = New Point(162, 42)
+        ButtonExportJson.Name = "ButtonExportJson"
+        ButtonExportJson.Size = New Size(150, 46)
+        ButtonExportJson.TabIndex = 4
+        ButtonExportJson.Text = "Export"
+        ButtonExportJson.UseVisualStyleBackColor = True
+        ' 
+        ' TreeViewTasks
+        ' 
+        TreeViewTasks.Location = New Point(3, 89)
+        TreeViewTasks.Name = "TreeViewTasks"
+        TreeViewTasks.Size = New Size(528, 395)
+        TreeViewTasks.TabIndex = 6
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(ButtonAddChild)
+        GroupBox1.Controls.Add(ButtonDeleteChecked)
+        GroupBox1.Controls.Add(ButtonAddParent)
+        GroupBox1.Location = New Point(552, 89)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(354, 239)
+        GroupBox1.TabIndex = 8
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Commands"
+        ' 
+        ' ButtonAddChild
+        ' 
+        ButtonAddChild.Location = New Point(29, 104)
+        ButtonAddChild.Name = "ButtonAddChild"
+        ButtonAddChild.Size = New Size(253, 46)
+        ButtonAddChild.TabIndex = 8
+        ButtonAddChild.Text = "Add child item"
+        ButtonAddChild.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDeleteChecked
+        ' 
+        ButtonDeleteChecked.Location = New Point(29, 156)
+        ButtonDeleteChecked.Name = "ButtonDeleteChecked"
+        ButtonDeleteChecked.Size = New Size(253, 44)
+        ButtonDeleteChecked.TabIndex = 3
+        ButtonDeleteChecked.Text = "Delete Item"
+        ButtonDeleteChecked.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonAddParent
+        ' 
+        ButtonAddParent.Location = New Point(29, 52)
+        ButtonAddParent.Name = "ButtonAddParent"
+        ButtonAddParent.Size = New Size(253, 46)
+        ButtonAddParent.TabIndex = 7
+        ButtonAddParent.Text = "Add parent item"
+        ButtonAddParent.UseVisualStyleBackColor = True
+        ' 
+        ' TaskManager
+        ' 
+        AutoScaleDimensions = New SizeF(13.0F, 32.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(956, 656)
+        Controls.Add(TableLayoutPanel1)
+        Name = "TaskManager"
+        Text = "Task Manager"
+        TableLayoutPanel1.ResumeLayout(False)
+        TableLayoutPanel1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox1.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TextBoxTaskName As TextBox
+    Friend WithEvents ButtonImportJson As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents ButtonExportJson As Button
+    Friend WithEvents TreeViewTasks As TreeView
+    Friend WithEvents ButtonAddParent As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ButtonAddChild As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ButtonDeleteChecked As Button
+
+End Class
---
+++ ToDoListApp/TaskManager.vb
@@ -0,0 +1,96 @@
+﻿Public Class TaskManager
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TreeViewTasks.CheckBoxes = True
+    End Sub
+
+    Private Sub ButtonDeleteChecked_Click(sender As Object, e As EventArgs) Handles ButtonDeleteChecked.Click
+        DeleteCheckedNodes(TreeViewTasks.Nodes)
+    End Sub
+
+    Private Sub DeleteCheckedNodes(nodes As TreeNodeCollection)
+        For i As Integer = nodes.Count - 1 To 0 Step -1
+            Dim node As TreeNode = nodes(i)
+            If node.Checked Then
+                nodes.Remove(node)
+            Else
+                DeleteCheckedNodes(node.Nodes)
+            End If
+        Next
+    End Sub
+
+    Private Sub ButtonExportJson_Click(sender As Object, e As EventArgs) Handles ButtonExportJson.Click
+        Dim tasks As New List(Of Object)
+        BuildTaskList(TreeViewTasks.Nodes, tasks, Nothing)
+
+        Dim json As String = Newtonsoft.Json.JsonConvert.SerializeObject(tasks, Newtonsoft.Json.Formatting.Indented)
+        System.IO.File.WriteAllText("tasks.json", json)
+        MessageBox.Show("Tâches exportées avec succès.")
+    End Sub
+
+    Private Sub BuildTaskList(nodes As TreeNodeCollection, tasks As List(Of Object), parentTask As Object)
+        For Each node As TreeNode In nodes
+            Dim task As New With {.Title = node.Text, .IsChecked = node.Checked, .Children = New List(Of Object)()}
+            If parentTask Is Nothing Then
+                tasks.Add(task)
+            Else
+                parentTask.Children.Add(task)
+            End If
+            If node.Nodes.Count > 0 Then BuildTaskList(node.Nodes, tasks, task)
+        Next
+    End Sub
+
+    Private Sub TreeViewTasks_AfterCheck(sender As Object, e As TreeViewEventArgs) Handles TreeViewTasks.AfterCheck
+        RemoveHandler TreeViewTasks.AfterCheck, AddressOf TreeViewTasks_AfterCheck
+        CheckAllChildNodes(e.Node, e.Node.Checked)
+        If e.Node.Parent IsNot Nothing Then CheckParentNode(e.Node.Parent)
+        AddHandler TreeViewTasks.AfterCheck, AddressOf TreeViewTasks_AfterCheck
+    End Sub
+
+    Private Sub CheckAllChildNodes(treeNode As TreeNode, nodeChecked As Boolean)
+        For Each node As TreeNode In treeNode.Nodes
+            node.Checked = nodeChecked
+            If node.Nodes.Count > 0 Then
+                CheckAllChildNodes(node, nodeChecked)
+            End If
+        Next
+    End Sub
+
+    Private Sub CheckParentNode(treeNode As TreeNode)
+        Dim allChecked As Boolean = True
+        For Each node As TreeNode In treeNode.Nodes
+            If Not node.Checked Then allChecked = False
+        Next
+        treeNode.Checked = allChecked
+        If treeNode.Parent IsNot Nothing Then CheckParentNode(treeNode.Parent)
+    End Sub
+
+    Private Sub ButtonAddParent_Click(sender As Object, e As EventArgs) Handles ButtonAddParent.Click
+        If Not String.IsNullOrWhiteSpace(TextBoxTaskName.Text) Then
+            TreeViewTasks.Nodes.Add(TextBoxTaskName.Text)
+            TextBoxTaskName.Clear()
+        End If
+    End Sub
+
+    Private Sub ButtonAddChild_Click(sender As Object, e As EventArgs) Handles ButtonAddChild.Click
+        If Not String.IsNullOrWhiteSpace(TextBoxTaskName.Text) AndAlso TreeViewTasks.SelectedNode IsNot Nothing Then
+            TreeViewTasks.SelectedNode.Nodes.Add(TextBoxTaskName.Text)
+            TreeViewTasks.SelectedNode.Expand()
+            TextBoxTaskName.Clear()
+        End If
+    End Sub
+
+    Private Sub AddNodes(tasks As IEnumerable(Of Object), nodes As TreeNodeCollection)
+        For Each task In tasks
+            Dim node As TreeNode = nodes.Add(task.Title)
+            node.Checked = task.IsChecked
+            If task.Children IsNot Nothing AndAlso DirectCast(task.Children, List(Of Object)).Count > 0 Then
+                AddNodes(task.Children, node.Nodes)
+            End If
+        Next
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
